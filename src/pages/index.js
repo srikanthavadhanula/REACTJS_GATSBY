1fff3eef50e4132921141d339313420d7452ab39
saisrikanth <saisrikanth@techmojo.in>
2020-04-18 22:26:46 +0530

    netlify error check

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 import HeroSection from "../components/Reusable/HeroSection"
 import Infoblock from "../components/Reusable/infoblock"
 import DualInfoBlock from "../components/Reusable/DualInfoBlock"
-import staffPic from "../images/srikanth.jpg"
+import staffPic from "../images/Srikanth.jpg"
 import Coursecart from "../components/Cart/Coursecart"
 
 const IndexPage = ({data}) => (
